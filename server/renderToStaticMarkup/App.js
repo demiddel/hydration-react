70e9863301ebf0359eebb685bbdf894d2bd9ff4e
MBing <martin.demiddel@idealo.de>
2023-10-18 20:05:53 +0200

    fix(rrd): more react router dom

--- server/renderToStaticMarkup/App.js
+++ server/renderToStaticMarkup/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import {
+  createStaticRouter,
+  StaticRouterProvider,
+  createStaticHandler,
+} from "react-router-dom/server";
+
 import Layout from "./shared/Layout";
 import Nav from "./shared/Nav";
 
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 
 const routes = [
   {
-    element: <Layout />,
+    Component: Layout,
+    ErrorBoundary: () => <div>Error Page</div>,
     children: [
       { path: "/", Component: App },
       { path: "/about", Component: About },
@@ -17,15 +23,15 @@ const routes = [
   },
 ];
 
-const router = createHashRouter(routes);
+const router = createStaticRouter(routes, { matches: [] });
 
 function App() {
   return (
-    <RouterProvider router={router}>
+    <StaticRouterProvider router={router} context={{}}>
       <Nav />
       <h1>My Site</h1>
       <p>Welcome to my website.</p>
-    </RouterProvider>
+    </StaticRouterProvider>
   );
 }
 
